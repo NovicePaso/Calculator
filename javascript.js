f4cd3a2ac3818161d0fa3a1e0471db55584cf6da
NovicePaso <135040767+NovicePaso@users.noreply.github.com>
2023-08-06 15:31:01 -0700

    Initialize variables, add button functionality Initialized first set of variables based off pseudocode outlined logic.  Added class names for each button and added eventlisteners for each button click. When each button is clicked, it will store the innerText of the button into a variable.

--- javascript.js
+++ javascript.js
@@ -1,19 +1,47 @@
 // establish and store events into variables (1st input, 2nd input)
 // concat values for multiple digits pressed before operator
+let inputValue1 = null;
+let inputValue2 = null;
+let operatorElement = "";
+let result = null;
 
 // button functionality; eventListener
 
-// divide function
+let operands = document.querySelectorAll(".operand");
+operands.forEach((e) => e.addEventListener("click", function operand(){
+    inputValue = Number(e.innerText);
+    console.log(inputValue);
+}))
 
-// multiply function
-
-// add function
-
-// subtract function
+let operators = document.querySelectorAll(".operator");
+operators.forEach((e) => e.addEventListener("click", function operator(){
+    operatorElement = (e.innerText);
+    console.log(operatorElement);
+}))
 
 // clear function
+document.querySelector(".clear-button").addEventListener("click", function clear(){
+    console.log("Insert Clear Function Here");
+})
+
+// divide function
+result = inputValue1 / inputValue2;
+
+// multiply function
+result = inputValue1 * inputValue2;
+
+// add function
+result = inputValue1 + inputValue2;
+
+// subtract function
+result = inputValue1 - inputValue2;
+
 
 // operate function for "="
+document.querySelector(".operate").addEventListener("click", function operate(){
+    console.log("Insert Operate Function Here");
+})
+// push results to display-screen
 
 // error proofing: 
 // enstablishing correct button order pressing rules
