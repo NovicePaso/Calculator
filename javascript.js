de014a5ad8e79b6fe64e6ab8e926655925fa0bd2
NovicePaso <135040767+NovicePaso@users.noreply.github.com>
2023-08-06 10:16:29 -0700

    Create boilerplate and pseudocode

---
+++ javascript.js
@@ -0,0 +1,22 @@
+// establish and store events into variables (1st input, 2nd input)
+// concat values for multiple digits pressed before operator
+
+// button functionality; eventListener
+
+// divide function
+
+// multiply function
+
+// add function
+
+// subtract function
+
+// clear function
+
+// operate function for "="
+
+// error proofing: 
+// enstablishing correct button order pressing rules
+// divide by 0 response
+// rounding decimals
+
