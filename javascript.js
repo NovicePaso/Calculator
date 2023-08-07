1b364a4daaeffeb7fdb786242315e2637cde1165
NovicePaso <135040767+NovicePaso@users.noreply.github.com>
2023-08-06 17:18:02 -0700

    Initialize basic functionality and logic paths
    Changes:
    - Initialize new variables
    - add concatination function to input values
    - establish logic pathways for various sequences
    - Add TO-DO's for error-proofing.  (To be developed
    in future revisions)
    - Add basic functionality

--- javascript.js
+++ javascript.js
@@ -1,22 +1,43 @@
 // establish and store events into variables (1st input, 2nd input)
-// concat values for multiple digits pressed before operator
-let inputValue1 = null;
-let inputValue2 = null;
+let inputValue1 = 0;
+let inputValue1changed = false;
+let inputValue2 = 0;
+let inputValue2changed = false;
 let operatorElement = "";
-let result = null;
+let result = 0;
 
 // button functionality; eventListener
-
+// concat values for multiple digits pressed before operator
+// TO-DO: if operator is selected after inputValue2, store initial operation into result
 let operands = document.querySelectorAll(".operand");
 operands.forEach((e) => e.addEventListener("click", function operand(){
-    inputValue = Number(e.innerText);
-    console.log(inputValue);
+    // if operand is clicked after operator, function will store into inputValue2
+    if (inputValue1changed == true){
+        inputValue2 = inputValue2 + e.innerText;
+        inputValue2 = Number(inputValue2);
+        inputValue2changed = true;
+        console.log(`inputValue2: ${inputValue2}`);
+        console.log(`inputValue2changed: ${inputValue2changed}`);
+    }
+    // if operand is clicked before operator, function will store into inputValue1
+    else if (inputValue1changed == false){
+            inputValue1 = inputValue1 + e.innerText;
+            inputValue1 = Number(inputValue1);
+            console.log(`inputValue1: ${inputValue1}`)
+            console.log(`inputValue1changed: ${inputValue1changed}`);
+    }
 }))
 
+// tells calculator inputValue1 has been changed after selecting an operator
+// TO-DO: test logic for inputValue1changed = false, operator selected as first input to calculator
 let operators = document.querySelectorAll(".operator");
 operators.forEach((e) => e.addEventListener("click", function operator(){
+    if (inputValue1changed == false){
+        inputValue1changed = true;
+    }
     operatorElement = (e.innerText);
-    console.log(operatorElement);
+    console.log(`operatorElement: ${operatorElement}`);
+    console.log(`inputValue1changed: ${inputValue1changed}`);
 }))
 
 // clear function
@@ -24,22 +45,32 @@ document.querySelector(".clear-button").addEventListener("click", function clear
     console.log("Insert Clear Function Here");
 })
 
-// divide function
-result = inputValue1 / inputValue2;
-
-// multiply function
-result = inputValue1 * inputValue2;
-
-// add function
-result = inputValue1 + inputValue2;
-
-// subtract function
-result = inputValue1 - inputValue2;
-
-
-// operate function for "="
+// operate functionality for "="
+// TO-DO: If = is pressed after first input value vs operator
 document.querySelector(".operate").addEventListener("click", function operate(){
-    console.log("Insert Operate Function Here");
+    if(inputValue2changed === true){    
+    switch (operatorElement){
+        case "/": 
+            result = inputValue1 / inputValue2;
+            break;
+        case "X":
+            result = inputValue1 * inputValue2;
+            break;
+        case "+":
+            result = inputValue1 + inputValue2;
+            break;
+        case "-":
+            result = inputValue1 - inputValue2;
+            break;
+    }
+
+    console.log(`inputValue1: ${inputValue1}`)
+    console.log(`inputValue1changed: ${inputValue1changed}`);
+    console.log(`operatorElement: ${operatorElement}`);
+    console.log(`inputValue2: ${inputValue2}`);
+    console.log(`inputValue2changed: ${inputValue2changed}`);
+    console.log(`Result: ${inputValue1} ${operatorElement} ${inputValue2} is ${result}`);
+}
 })
 // push results to display-screen
 
