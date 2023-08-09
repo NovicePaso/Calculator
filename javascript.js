427229171e9d5a6047735bf22d84c0aa079e26c1
NovicePaso <135040767+NovicePaso@users.noreply.github.com>
2023-08-08 18:04:42 -0700

    Clean up code and fix bugs Separated functions and variables at the top, and eventListeners at the bottom.  Fixed a bug where clicking "/" multiple times would affect the variables unintentionally.  Fixed another bug where multiplying a decimal with a large value results in a large number of decimals.  Changed variables being displayed from console.log to responsive "screens".
    Cleaned up code to have variables and functions at the top, and
    button eventListeners and function calls at the bottom.  Overall,
    much more digestable to look at.
    
    Added deparate divs for Input and Result screens vs
    displays.

--- javascript.js
+++ javascript.js
@@ -1,49 +1,63 @@
-// establish and store events into variables (1st input, 2nd input)
-inputValue1 = 0;
-inputValue1changed = false;
-inputValue2 = 0;
-inputValue2changed = false;
-operatorElement = null;
-result = 0;
+let inputValue1 = 0;
+let inputValue1changed = false;
+let inputValue2 = 0;
+let inputValue2changed = false;
+let operatorElement = null;
+let result = 0;
 
-// operate functionality
+
 function operate(){
     if (inputValue2changed == false && inputValue1changed == false){
         result = inputValue1;
-        console.log(`result is: ${result}`);
+        document.querySelector(".input-screen").innerText = inputValue1;
+        document.querySelector(".result-screen").innerText = result;
     }
     else if(inputValue2changed === true){    
-    switch (operatorElement){
-        case "/": 
-            result = Math.round((inputValue1 / inputValue2)*100) / 100;
-            break;
-        case "X":
-            result = inputValue1 * inputValue2;
-            break;
-        case "+":
-            result = inputValue1 + inputValue2;
-            break;
-        case "-":
-            result = inputValue1 - inputValue2;
-            break;
-    }
-    if(result === Infinity || NaN){
-        console.log("How dare you.");
-    }
+        switch (operatorElement){
+            case "/": 
+                result = Math.round((inputValue1 / inputValue2)*100)/100;
+                break;
+            case "X":
+                result = Math.round((inputValue1 * inputValue2)*100)/100;
+                break;
+            case "+":
+                result = inputValue1 + inputValue2;
+                break;
+            case "-":
+                result = inputValue1 - inputValue2;
+                break;
+        }
 
-    console.log(`inputValue1: ${inputValue1}`)
-    console.log(`inputValue1changed: ${inputValue1changed}`);
-    console.log(`operatorElement: ${operatorElement}`);
-    console.log(`inputValue2: ${inputValue2}`);
-    console.log(`inputValue2changed: ${inputValue2changed}`);
-    console.log(`Result: ${inputValue1} ${operatorElement} ${inputValue2} is ${result}`);
-inputValue1 = result;
-inputValue2 = 0;
-operatorElement = null;
-}
+        console.log(`inputValue1: ${inputValue1}`)
+        console.log(`inputValue1changed: ${inputValue1changed}`);
+        console.log(`operatorElement: ${operatorElement}`);
+        console.log(`inputValue2: ${inputValue2}`);
+        console.log(`inputValue2changed: ${inputValue2changed}`);
+        document.querySelector(".result-screen").innerText = result;
+        inputValue1 = result;
+        inputValue2 = 0;
+        operatorElement = null;
+
+        if(result == Infinity || NaN){
+            document.querySelector(".result-screen").innerText = "How dare you.  I'm resetting now."
+            result = 0;
+        }
+    }
 }
 
-// button functionality; eventListener
+function clear(){
+    inputValue1 = 0;
+    inputValue1changed = false;
+    inputValue2 = 0;
+    inputValue2changed = false;
+    operatorElement = null;
+    result = 0;
+    console.log(inputValue1);
+    document.querySelector(".input-screen").innerText = ``;
+    document.querySelector(".result-screen").innerText = result;
+} 
+
+// button functionality; categorizes inputs into "operand", "operator", "operate", or "clear" categories
 operands = document.querySelectorAll(".operand");
 operands.forEach((e) => e.addEventListener("click", function operand(){
     // if operand is clicked after operator, function will store into inputValue2
@@ -53,6 +67,7 @@ operands.forEach((e) => e.addEventListener("click", function operand(){
         inputValue2changed = true;
         console.log(`inputValue2: ${inputValue2}`);
         console.log(`inputValue2changed: ${inputValue2changed}`);
+        document.querySelector(".input-screen").innerText = `${inputValue1} ${operatorElement} ${inputValue2}`;
     }
     // if operand is clicked before operator, function will store into inputValue1
     else if (inputValue1changed == false){
@@ -60,36 +75,31 @@ operands.forEach((e) => e.addEventListener("click", function operand(){
             inputValue1 = Number(inputValue1);
             console.log(`inputValue1: ${inputValue1}`)
             console.log(`inputValue1changed: ${inputValue1changed}`);
+            document.querySelector(".input-screen").innerText = inputValue1;
     }
 }))
 
-// tells calculator inputValue1 has been changed after selecting an operator
 operators = document.querySelectorAll(".operator");
 operators.forEach((e) => e.addEventListener("click", function operator(){
+    // when operator is selected first
     if (inputValue1changed == false){
-        inputValue1changed = true;
+        inputValue1changed = true
+        result = inputValue1;
+        document.querySelector(".input-screen").innerText = `${inputValue1} ${operatorElement}`;;
     }
+    // when operator is selected after inputValue2 has been logged
     else if (inputValue1changed == true && inputValue2changed == true){
         operate();
     }
     operatorElement = (e.innerText);
+    document.querySelector(".input-screen").innerText = `${inputValue1} ${operatorElement}`;
     console.log(`operatorElement: ${operatorElement}`);
     console.log(`inputValue1changed: ${inputValue1changed}`);
 }))
 
-// clear function
-document.querySelector(".clear-button").addEventListener("click", function clear(){
-    inputValue1 = 0;
-    inputValue1changed = false;
-    inputValue2 = 0;
-    inputValue2changed = false;
-    operatorElement = null;
-    result = 0;
-console.log(inputValue1);
-})
-
-
 document.querySelector(".operate").addEventListener("click", operate)
+
+document.querySelector(".clear-button").addEventListener("click", clear)
 
 
 // push results to display-screen
